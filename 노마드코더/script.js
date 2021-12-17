0fc49b26022d956c6a9580a78da4df81a2c5b87d
zero_youth <86909942+zeroyouth@users.noreply.github.com>
2021-12-17 23:11:55 +0900

    12/17

---
+++ 노마드코더/script.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function loginSubmit() {
+    const username = loginInput.value; //
+    console.log(username);
+}
+
+loginForm.addEventListener("submit", loginSubmit);
+
